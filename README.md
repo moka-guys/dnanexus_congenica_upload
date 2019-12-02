a2f899d1243a80d5cdc361ae544630b4082df618
Aled Jones <aledjones@nhs.net>
2019-12-02 14:22:07 +0000

    revise to sinplest use case

--- README.md
+++ README.md
@@ -1,48 +1,46 @@
 # Sapientia upload v1.0
+
 ## What does this app do?
+
 This app uploads samples to Congenica's Sapientia analysis platform. This uses a dockerised upload client provided by Congenica.
 
 ## What are typical use cases for this app?
-Following the completion of the bioinformatic workflow, variant calls are uploaded for annotation and prioritisation in the Sapientia analysis platform.
-This app supports two seperate workflows:
-1) Upload singleton (affected) samples without hpo terms - in this case an ir.csv file is not provided (it's created on the fly) and each sample is uploaded individually.
-2) Upload of one or more samples using a provided ir.csv file. The required fields (file paths) are filled in and all samples are uploaded in a single upload. This can be used for trios/related samples or where gene panels or HPO terms are required.
 
+Following the completion of the bioinformatic workflow, variant calls are uploaded for annotation and prioritisation in the Sapientia analysis platform.
+This app currently supports the following scenarios:
+
+1) Upload singleton (affected) sample without hpo terms - in this case an ir.csv file is not provided (it's created on the fly).
 
 ## What inputs are required for this app to run?
+
 This app requires the following inputs:
-- **VCF file** - one or more VCFs can be provided
+
+- **VCF file** - One single sample VCF file.
 - **Project ID** of Sapientia project to upload samples
 
 Optional inputs
+
 - **BAM file(s)** (`*.bam`). Only BAM files with an associated VCF file will be imported.  
-- An **analysis name** - This analysis name is displayed in Sapientia. By default this is taken from the name of the NGS run.
-- An **ir.csv** - This file describes all the information for one or more samples, including hpo terms, proband, family structure.
-If ir.csv file is provided an analysis name is required.
+- An **analysis name** - This analysis name is displayed in Sapientia. By default this is the samplename.
 
 The app downloads the sapientia credentials (`sapientia_env`) from 001_Authentication.
 
 ## How does this app work?
+
 The inputs and credentials files are downloaded.
 The docker image provided by Congenica is used to upload samples and files to the platform. The upload client performs a number of checks, such as comparison of samplename and the samplename in the VCF file. This requires an analysis id and ir.csv file.
 
 The IR.csv file is required by the upload client to link files to each analysis and to add meta data such as sex and family structure.
-If not provided this file is created by looping through all VCF inputs extracting the samplename, sex, VCF and BAM files (if provided).
+This file is created by parsing the VCF file name and extracting the samplename, sex and capturing paths to VCF and BAM files (if provided).
 The sex is identified using the single letter sex in the samplename. If it is neither "F" or "M" it is treated as unknown. This is case sensitive.
-One upload is performed per sample and if, not provided, the analysis id is taken from the samplename
+If not provided, the analysis id is taken from the samplename
 
-If the IR.csv file is provided the file paths are filled in. Where the ir.csv file is provided the analysis ID is also required
-One upload is done per run.
+The congenica upload client produces a log file. This is uploaded from the job (but not as an output in order to see why the upload failed). If any uploads are unsucessful the app will fail.
 
-The congenica upload client produces one log file per upload. This is uploaded (but not as an output) in order to see why the upload failed. If any uploads are unsucessful the app will fail.
+## What does this app output
 
+The upload client produces a log file ($analysis_name_upload.log).
+The completed ir.csv is also output (named $analysis_name_ir.csv) .
+These log files are output to /sapientia_logs/
 
-## What does this app output?
-The upload client produces a log file ($analysis_name_upload.log) per upload. 
- 
-The log files are output to /sapientia_logs/
-
-The completed ir.csv is also output (named $analysis_name_ir.csv) to the same folder.
-
-
-## This app was made by Viapath Genome Informatics 
+## This app was made by Viapath Genome Informatics
--- src/code.sh
+++ src/code.sh
@@ -17,151 +17,64 @@ mark-section "setting up congenica upload client docker image"
 docker load -i '/home/dnanexus/congenica-client-2.2.0.0_3.tgz' 
 
 mark-section "determine run specific variables"
-# get project - this is an string input
+# get sapientia project - this is an string input
 echo "sapientia project = " $sapientia_project
 
 # make output folders for log file and ir_file
 mkdir -p ~/out/logfile/sapientia_logs/ ~/out/ir_file/sapientia_logs
 
-# 2 use cases - 
-# 1) upload singleton (affected) samples without hpo terms - in this case an ir.csv file is not provided and it's created on the fly for each sample and uploaded individually.
-# 2) upload of WES cases (singletons or families) where HPO terms and affected status are required. In this case the ir.csv file is provided and the required fields (file paths) are filled in. There is one upload per app.
+# Use case 1 - upload singleton (affected) samples without hpo terms
+# in this case an ir.csv file is not provided and it's created on the fly
+mark-section "Build ir.csv file"
+     
+# extract the sample name to match that in the vcf by taking everything before "_R1" eg (NGS282rpt_16_136819_NA12878_F_WES47_Pan493_S16from NGS282rpt_16_136819_NA12878_F_WES47_Pan493_S16_R1_001.refined.vcf.gz)
+samplename=$(python -c "basename='$vcf_prefix'; print basename.split('_R1')[0].split('.vcf')[0]")
+echo $samplename
+if [[ $analysis_name == "" ]]
+then 
+    analysis_name=$samplename
+fi
+# extract 5th item (single character sex) and extrapolate into full word. If not "M" or "F" set as unknown
+sex_singlechar=$(echo $(basename $vcf) | cut -d"_" -f5); 
+if [[ $sex_singlechar == "F" ]]
+    then 
+        sex="female"
+elif [[ $sex_singlechar == "M" ]]
+    then 
+        sex="male"
+else 
+    sex="unknown"
+fi
 
-# if the ir_csv has not been provided
-if [[ ! -f $ir_csv_path ]]
+mark-section "matching up BAM files"
+# look if sample name (created above) is present in the bamfile path.
+if [[ "$bam_path" == *$samplename* ]]
 then
-    # upload each sample
-    mark-section "upload individual samples by building ir.csv file"
-    # need to populate sample name, sex, bamfile and vcf file paths for each vcf
-    for vcf in ${vcfs_path[@]}
-    do
-        #  Build ir.csv file for this sample        
-        mark-section "Building ir.csv"
-        
-        # extract the sample name to match that in the vcf by taking everything before .vcf (NGS282rpt_16_136819_NA12878_F_WES47_Pan493_S16)
-        vcf_basename=$(basename $vcf)
-        samplename=$(python -c "basename='$vcf_basename'; print basename.split('_R1')[0].split('.vcf)[0]'")
-        echo $samplename
-        if [[ $analysis_name == "" ]]
-        then 
-            analysis_name=$samplename
-        fi
-        # extract 5th item (single character sex) and extrapolate into full word. If not "M" or "F" set as unknown
-        sex_singlechar=$(echo $(basename $vcf) | cut -d"_" -f5); 
-        if [[ $sex_singlechar == "F" ]]
-            then 
-                sex="female"
-        elif [[ $sex_singlechar == "M" ]]
-            then 
-                sex="male"
-        else 
-            sex="unknown"
-        fi
-        mark-section "matching up BAM files"
-        #create variable to hold bamfile name - starts off empty. Once match has been made it will stop searching.
-        bamfile=""
-        # for this vcf look to see if there is a BAM file.
-        for bam in ${bams_path[@]}
-        do
-            echo "looking for samplename $samplename in bamfile path $bam"
-            # look if sample name (created above) is present in the bamfile path.
-            if [[ "$bam" == *$samplename* ]]
-            then
-                bamfile=$bam
-                echo "found matching BAM"
-            fi
-        done
-        # make a copy of the ir.csv file containing header
-        cp ~/ir_file.csv ~/out/ir_file/sapientia_logs/$samplename.csv
-        # write the sample details to the ir csv file
-        echo "$samplename,$sex,,affected,1,,,,,,,,$bamfile,,,,$vcf," >> ~/out/ir_file/sapientia_logs/$samplename.csv
+    bamfile=$bam_path
+    echo "found matching BAM"
+fi
 
-        # cat the ir.csv file so it can be seen in the logs for easy troubleshooting (is also an output but will not be output if job fails)
-        cat ~/out/ir_file/sapientia_logs/$samplename.csv
+# make a copy of the packaged ir.csv template file containing header
+cp ~/ir_file.csv ~/out/ir_file/sapientia_logs/$samplename.csv
+# write the sample details to the ir csv file
+echo "$samplename,$sex,,affected,1,,,,,,,,$bamfile,,,,$vcf_path," >> ~/out/ir_file/sapientia_logs/$samplename.csv
 
-        mark-section "upload using docker image"
-        # docker run - mount the home directory as a share, use the env_file, ir_file.csv, $sapientia_project and $analysis_name values determined above. 
-        # Write log direct into output folder
-        docker run -v /home/dnanexus/:/home/dnanexus/ --env-file ~/env_file congenica-client:2.2.0.0_3 --ir ~/out/ir_file/sapientia_logs/$samplename.csv --project $sapientia_project --name $analysis_name --log ~/out/ir_file/sapientia_logs/"$analysis_name"_upload.log
-        docker_status=$?
-        if [ $docker_status -ne 0 ]
-        then
-            successful_app_run=$docker_status
-            cat /home/dnanexus/out/logfile/sapientia_logs/"$analysis_name"_upload.log
-        fi 
-    done
-# if ir_csv file has been provided
-else
-    # if ir.csv file given the analysis name is also required - put a test in here.
-    
-    mark-section "upload samples using provided ir.csv file"
-    # need to populate filepaths for each sample
-    # loop through provided ir.csv one line at a time
-    while read line
-    do 
-        # capture the samplename, from first field of the line
-        vcf_basename=$(basename $vcf)
-        samplename=$(python -c "basename='$vcf_basename'; print basename.split('_R1')[0].split('.vcf')[0]")
-        # set up variables to hold filepaths
-        vcffile=""
-        bamfile=""
-        # loop through all input vcfs
-        for vcf in ${vcfs_path[@]}
-        do
-            # if the samplename is in the filename capture the filepath
-            if [[ "$vcf" == *$samplename* ]]
-            then
-                vcffile=$vcf
-            fi
-        done
-        echo $vcffile
-        # loop through all input bams
-        for bam in ${bams_path[@]}
-        do
-            if [[ "$bam" == *$samplename* ]]
-            then
-                # if the samplename is in the filename capture the filepath
-                bamfile=$bam
-            fi
-        done
+# cat the ir.csv file so it can be seen in the logs for easy troubleshooting (is also an output but will not be output if job fails)
+cat ~/out/ir_file/sapientia_logs/$samplename.csv
 
-        # if it's the header of the ir.csv write the line to a new file
-        if [[ "$samplename" == "name" ]]
-        then
-            echo $line > ~/out/ir_file/sapientia_logs/$analysis_name.csv
-        else
-            # test to ensure vcf file has been detected
-            if [[ "$vcffile" = "" ]]
-            then
-                echo "vcf file not detected for samplename " $samplename
-                exit 1
-            fi
-            # if it's not header write line but replace placeholders withfilepaths
-            if [[ $bamfile -ne "" ]]
-            then 
-                echo  $line | sed "s+bam_path++" | sed "s+vcf_path+$vcffile+" >> ~/out/ir_file/sapientia_logs/$analysis_name.csv
-            else
-                echo  $line | sed "s+bam_path+$bamfile+" | sed "s+vcf_path+$vcffile+" >> ~/out/ir_file/sapientia_logs/$analysis_name.csv
-            fi
-        fi
-    # pass the provided ir.csv file into the above if loop
-    done < $ir_csv_path
-
-    
-    # cat the ir.csv file so it can be seen in the logs for easy troubleshooting (is also an output but will not be output if job fails)
-    cat ~/out/ir_file/sapientia_logs/$analysis_name.csv
-
-    mark-section "upload using docker image"
-    # docker run - mount the home directory as a share, use the env_file, ir_file.csv, $sapientia_project and $analysis_name values determined above. 
-    # Write log direct into output folder
-    docker run -v /home/dnanexus/:/home/dnanexus/ --env-file ~/env_file congenica-client:2.2.0.0_3 --ir ~/out/ir_file/sapientia_logs/$analysis_name.csv --project $sapientia_project --name $analysis_name --log ~/out/ir_file/sapientia_logs/"$analysis_name"_upload.log
-    docker_status=$?
-    if [ $docker_status -ne 0 ]
-    then
-        cat /home/dnanexus/out/logfile/sapientia_logs/"$analysis_name"_upload.log
-        successful_app_run=$docker_status
-    fi
+mark-section "upload using docker image"
+# docker run - mount the home directory as a share, use the env_file, ir_file.csv, $sapientia_project and $analysis_name values determined above. 
+# Write log direct into output folder
+docker run -v /home/dnanexus/:/home/dnanexus/ --env-file ~/env_file congenica-client:2.2.0.0_3 --ir ~/out/ir_file/sapientia_logs/$samplename.csv --project $sapientia_project --name $analysis_name --log ~/out/ir_file/sapientia_logs/"$analysis_name"_upload.log
+docker_status=$?
+if [ $docker_status -ne 0 ]
+then
+    successful_app_run=$docker_status
+    cat /home/dnanexus/out/logfile/sapientia_logs/"$analysis_name"_upload.log
 fi 
+
+# cat the ir.csv file so it can be seen in the logs for easy troubleshooting (is also an output but will not be output if job fails)
+cat ~/out/ir_file/sapientia_logs/$analysis_name.csv
 
 mark-section "Upload output"
 # to do dx upload need to reset worker variable
@@ -170,7 +83,7 @@ unset DX_WORKSPACE_ID
 dx cd $DX_PROJECT_CONTEXT_ID:
 ls ~/out/ir_file/sapientia_logs
 # make folder for output - use -p so doesn't fail if file already exists
-dx -p mkdir sapientia_logs
+dx mkdir -p sapientia_logs
 # run dx uplaod command to the desired location - upload all files in the sapientia_logs output folder 
 dx upload --brief --path "$DX_PROJECT_CONTEXT_ID:/sapientia_logs/" ~/out/ir_file/sapientia_logs/*
 
